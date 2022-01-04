17904991932ea19dfe6495a4f16bc89dd857774d
naveen741 <96760074+naveen741@users.noreply.github.com>
2022-01-04 10:19:37 +0530

    Update script.js

--- Task-2/form/script.js
+++ Task-2/form/script.js
@@ -3,47 +3,65 @@
     let i=0;
     myForm.addEventListener("submit",function(evt){
         evt.preventDefault();
-        const myName=document.getElementById("name").value;
-        const myLName=document.getElementById("l-name").value;
-        const myDOB=document.getElementById("dob").value;
-        const myNation=document.getElementById("nation").value;
-        const myNum=document.getElementById("number").value;
-        const myAdd=document.getElementById("address").value;
+        const myName=document.getElementById("name");
+        const myLName=document.getElementById("l-name");
+        const myDOB=document.getElementById("dob");
+        const male=document.getElementById("male");
+        const female=document.getElementById("female");
+        let gen;
+        const myNation=document.getElementById("nation");
+        const myNum=document.getElementById("number");
+        const myAdd=document.getElementById("address");
         const myCon=document.getElementsByClassName("subContainer");
         const myContent=document.createElement("div");
         const name=document.createElement("h2")
         const lname=document.createElement("h2")
         const DOB=document.createElement("h2")
+        const gender=document.createElement("h2")
         const nation=document.createElement("h2")
         const num=document.createElement("h2")
         const add=document.createElement("h2")
         const myDiv=myCon[i].getElementsByTagName("div");
-        name.innerText=`First Name : ${myName}`;
+        
+        
+        
+        name.innerText=`First Name : ${myName.value}`;
         myContent.appendChild(name);
         
-        lname.innerText=`Last Name : ${myLName}`;
+        lname.innerText=`Last Name : ${myLName.value}`;
         myContent.appendChild(lname);
         
-        DOB.innerText=`DOB : ${myDOB}`;
+        DOB.innerText=`DOB : ${myDOB.value}`;
         myContent.appendChild(DOB);
-        
-        nation.innerText=`Nationality : ${myNation}`;
+        if(male.checked){
+            gen=male.value;
+        }
+        else if(female.checked){
+            gen=female.value;
+        }
+
+        gender.innerText=`Gender : ${gen}`;
+        myContent.appendChild(gender);
+
+        nation.innerText=`Nationality : ${myNation.value}`;
         myContent.appendChild(nation);
         
-        num.innerText=`Mobile Number : ${myNum}`;
+        num.innerText=`Mobile Number : ${myNum.value}`;
         myContent.appendChild(num);
         
-        add.innerText=`Address : ${myAdd}`;
+        add.innerText=`Address : ${myAdd.value}`;
         myContent.appendChild(add);
 
         myCon[i].replaceChild(myContent,myDiv[0]);
         //myCon[i].appendChild(myContent);
-        document.getElementById("name").value="";
-        document.getElementById("l-name").value="";
-        document.getElementById("dob").value="";
-        document.getElementById("nation").value="";
-        document.getElementById("number").value="";
-        document.getElementById("address").value="";
+        myName.value="";
+        myLName.value="";
+        myDOB.value="";
+        myNation.value="";
+        myNum.value="";
+        myAdd.value="";
+        male.checked=false;
+        female.checked=false;
         if(i==0){            
             i=2;
         }
