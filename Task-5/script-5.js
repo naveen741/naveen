ab79d8d030968a0c118b7d905e168d1f5d3a1e7c
naveen741 <96760074+naveen741@users.noreply.github.com>
2022-01-11 15:29:56 +0530

    Update script-5.js

--- Task-5/script-5.js
+++ Task-5/script-5.js
@@ -1,29 +1,30 @@
 const images=["image1.jpg","image2.jpg","image3.jpg","image4.jpg","image5.jpg"]
-        const Mynext=document.getElementById("next");
-        const Myprevious=document.getElementById("prev");
-        let i=0;
-        const myNav=document.querySelectorAll(".navi");
-        myNav[0].setAttribute("style","background-color: #f0f8ff;");
-        Mynext.addEventListener('click',function(){
-            i++;
-            if(i>images.length-1){i=0;}
-            change(i);
-        });
-        Myprevious.addEventListener('click',function(){
-            i--;
-            if(i<0){i=images.length-1;}
-            change(i);
-        });
-        function change(i){
-            const MyImage=document.getElementById("images");
-            const myNav=document.querySelectorAll(".navi");
-            MyImage.setAttribute('src',`./images/${images[i]}`);
-            for(let x=0;x<myNav.length;x++){
-                if(i==x){
-                    myNav[x].setAttribute("style","background-color: #f0f8ff;");
-                }
-                else{
-                    myNav[x].setAttribute("style","background-color: #000;");
-                }
-            }
+const Mynext=document.getElementById("next");
+const Myprevious=document.getElementById("prev");
+let i=0;
+const myNav=document.querySelectorAll(".navi");
+myNav[0].setAttribute("style","background-color: #f0f8ff;");
+Mynext.addEventListener('click',function(){
+    i++;
+    if(i>images.length-1){i=0;}
+    change(i);
+});
+Myprevious.addEventListener('click',function(){
+    i--;
+    if(i<0){i=images.length-1;}
+    change(i);
+});
+function change(y){
+    i=y;
+    const MyImage=document.getElementById("images");
+    const myNav=document.querySelectorAll(".navi");
+    MyImage.setAttribute('src',`./images/${images[y]}`);
+    for(let x=0;x<myNav.length;x++){
+        if(y==x){
+            myNav[x].setAttribute("style","background-color: #f0f8ff;");
         }
+        else{
+            myNav[x].setAttribute("style","background-color: #000;");
+        }
+    }
+}
