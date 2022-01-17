d7418fb48e6ae3102e3d3f24629ed7a174dd186c
naveen741 <96760074+naveen741@users.noreply.github.com>
2022-01-17 16:12:29 +0530

    Update script-6.js

--- Task-6/script-6.js
+++ Task-6/script-6.js
@@ -1,6 +1,5 @@
 var myTabs = document.querySelectorAll(".collapseTab");
-var i;
-for (i = 0; i < myTabs.length; i++) {
+for (let i = 0; i < myTabs.length; i++) {
 myTabs[i].addEventListener("click", function() {
     var cont = this.nextElementSibling;
     if (cont.style.maxHeight){
