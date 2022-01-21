916b7d81f5223899c12d7de04716393572f3c557
naveen741 <96760074+naveen741@users.noreply.github.com>
2022-01-21 10:18:03 +0530

    Update script-5.js

--- Task-5/script-5.js
+++ Task-5/script-5.js
@@ -1,14 +1,16 @@
-const images=["image1.jpg","image2.jpg","image3.jpg","image4.jpg","image5.jpg"]
+const images=["image1.jpg","image2.jpg","image3.jpg","image4.jpg","image5.jpg"];
 const Mynext=document.getElementById("next");
 const Myprevious=document.getElementById("prev");
 let i=0;
 const myNav=document.querySelectorAll(".navi");
 myNav[0].setAttribute("style","background-color: #f0f8ff;");
-Mynext.addEventListener('click',function(){
+setInterval(next,4000);/*for automatic sliding*/
+function next(){
     i++;
     if(i>images.length-1){i=0;}
     change(i);
-});
+}
+Mynext.onclick=next;
 Myprevious.addEventListener('click',function(){
     i--;
     if(i<0){i=images.length-1;}
@@ -17,7 +19,6 @@ Myprevious.addEventListener('click',function(){
 function change(y){
     i=y;
     const MyImage=document.getElementById("images");
-    const myNav=document.querySelectorAll(".navi");
     MyImage.setAttribute('src',`./images/${images[y]}`);
     for(let x=0;x<myNav.length;x++){
         if(y==x){
